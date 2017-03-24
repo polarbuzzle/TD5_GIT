c4b420a0fcd11f1d5b27fc586c6496658bcbc0e5
polarbuzzle <fersamw@gmail.com>
2017-03-23 21:56:01 -0800

    Reste affichage depile

--- partie 1/Pile.h
+++ partie 1/Pile.h
@@ -1,23 +1,60 @@
 #pragma once
+#include <vector>
 
 template <typename T1>
 class Pile {
 
 public:
 	//Constructeurs et destructeur
-	Pile();
-	Pile(int capacite);
-	~Pile();
+	Pile() : capacite_(10), nombreElements_(0) {
+		elements_ = new T1[10];
+	};
 
+	Pile(int capacite) : capacite_(capacite), nombreElements_(0) {
+		elements_ = new T1[capacite];
+	};
+	~Pile() {
+		delete[] elements_;
+	};
 
+	bool empiler(T1 premier) {
+		if (capacite_ != nombreElements_)
+		{
+			elements_[nombreElements_++] = premier;
+			return true;
+		}
+		return false;
+	};
 
-	bool empiler(T1 premier);
-	bool depiler();
+	bool depiler() {
+		if (nombreElements_ != 0)
+		{
+			--nombreElements_;
+			return true;
+		}
+		return false;
+	};
 
-	bool estVide() const;
-	bool estPleine() const;
-	T1* obtenirSommet() const;
-	int obtenirTaille() const;
+	bool estVide() const {
+		if (nombreElements_ == 0) {
+			return true;
+		}
+		return false;
+	};
+
+	bool estPleine() const {
+		if (capacite_ == nombreElements_)
+			return true;
+		return false;
+	};
+
+	T1* obtenirSommet() const {
+		return &elements_[nombreElements_];
+	};
+
+	int obtenirTaille() const {
+		return nombreElements_;
+	};
 
 private:
 
--- partie 1/main.cpp
+++ partie 1/main.cpp
@@ -12,24 +12,41 @@ int main()
     double durees[7] = {1.2, 3.5, 0.8, 2.5, 1.8, 0.75, 10};
     
     // Creer une pile de type "paires STL de (string, double)"
-    
+	Pile<pair<string, double> > pile(6);
+
     cout << "Empilage des taches..." << endl;
     // Ajouter les 7 paires correspondant aux tâches et durées ci-haut sur la pile.
     // Vous pouvez ici utiliser une boucle for standard de i = 0 à 7
     // Le programme doit afficher: "Tache ajoutee sur la pile: [nom de la tâche] d'une duree de [durée]"
     //                         ou: "La pile est pleine!"
-    //                         selon le succès ou non de l'ajout.
+    //                         selon le succès ou non de l'ajout.	
+	for (int i = 0; i < 7; i++) {
+		if (pile.empiler(make_pair(taches[i], durees[i])))
+		{
+			cout << "Tache ajoutee sur la pile: " << taches[i] << " d'une duree de " << durees[i] << endl;
+		}
+		else 
+		{
+			cout << "La pile est pleine!" << endl;
+		}
+	}
 
-	Pile<pair <string, double> > pile;
-
-        
     cout << "Depilage des taches..." << endl;
     // Retirer les 7 paires de (tâche,durée) de la pile, ***en assumant que vous ne connaissez pas le nombre d'éléments dans la pile***
     // (utilisez le booléen retourné par la fonction depiler())
     // Le programme doit afficher: "Depilage de la tache: [nom de la tâche] d'une duree de [durée]"
     //                         ou: "La pile est vide!"
     //                         selon le cas.
-
+	for (int i = pile.obtenirTaille(); i >= 0; i--) {
+		if (!pile.estVide()) {
+			 pair<string,double> jacques = *pile.obtenirSommet();
+			 string paul = jacques.first;
+			//cout << "Depilage de la tache " << pile.obtenirSommet().first;//<< "d'une duree de " << pile.obtenirSommet()->second << endl;
+			pile.depiler();
+		}
+		else
+			cout << "La pile est vide" << endl;
+	}
     
 
 
