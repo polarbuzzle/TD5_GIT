5068494399abee16c2dfff531ba65907073b3d11
polarbuzzle <fersamw@gmail.com>
2017-03-23 19:15:31 -0800

    Pile.h et ajout du pdf

---
+++ partie 1/Pile.h
@@ -0,0 +1,26 @@
+#pragma once
+
+template <typename T1>
+class Pile {
+
+public:
+	//Constructeurs et destructeur
+	Pile();
+	~Pile();
+
+
+	bool empiler(T1 premier);
+	bool depiler();
+
+	bool estVide() const;
+	bool estPleine() const;
+	T1* obtenirSommet() const;
+	int obtenirTaille() const;
+
+private:
+
+	int capacite_;
+	int nombreElements_;
+	T1* elements_;
+
+};
--- partie 1/main.cpp
+++ partie 1/main.cpp
@@ -13,13 +13,14 @@ int main()
     
     // Creer une pile de type "paires STL de (string, double)"
     
-
     cout << "Empilage des taches..." << endl;
     // Ajouter les 7 paires correspondant aux tâches et durées ci-haut sur la pile.
     // Vous pouvez ici utiliser une boucle for standard de i = 0 à 7
     // Le programme doit afficher: "Tache ajoutee sur la pile: [nom de la tâche] d'une duree de [durée]"
     //                         ou: "La pile est pleine!"
     //                         selon le succès ou non de l'ajout.
+
+	Pile<pair <string, double>> pile();
 
         
     cout << "Depilage des taches..." << endl;
